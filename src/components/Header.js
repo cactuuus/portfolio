78e0e1fb0999fad69424f50efdef43631db360c3
cactus <jacopo.bob.calvi@gmail.com>
2024-01-30 12:02:35 +0000

    re-styled mobile header nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
-import { NavMobile, NavDesktop } from "./Nav";
+import { SocialsDesktop, SocialsMobile } from "./Socials";
 
 const Header = ({ pages }) => {
   // true when under the treshold
@@ -10,10 +10,19 @@ const Header = ({ pages }) => {
     <header className="highlight">
       <Link to={"/"}>
         <h1 id="banner" className="link">
-          Jacopo Calvi
+          {isMobile ? "J.C." : "Jacopo Calvi"}
         </h1>
       </Link>
-      {isMobile ? <NavMobile pages={pages} /> : <NavDesktop pages={pages} />}
+      <nav>
+        {pages.map((link) => {
+          return (
+            <NavLink key={link.name} to={link.path} className="link">
+              {link.name}
+            </NavLink>
+          );
+        })}
+        {isMobile ? <SocialsMobile /> : <SocialsDesktop />}
+      </nav>
     </header>
   );
 };
