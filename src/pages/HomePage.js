90933d9524d7b3aa9c09409809d02925b62aa997
Jacopo Calvi <59688601+cactuuus@users.noreply.github.com>
2025-09-28 14:42:29 +0100

    updated old text (leetcode days are gone)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -18,25 +18,17 @@ const HomePage = () => {
         <article className="text-box sectioned">
           <FontAwesomeIcon className="icon grow" icon={faCode} />
           <p>
-            I do little websites like this one for fun, and other stuff too.{" "}
-            <b>Check out my projects page</b> to see what I’ve been up to, or
-            have a look at my current{" "}
-            <a
-              className="link"
-              href="https://leetcode.com/u/user8343Sz/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              LeetCode
-            </a>{" "}
-            status!
+            I like tinkering with websites and servers. When I'm not hitting my head against the wall, it is actually
+            quite pleasant!
+            <br/>
+            <b>Check out my project's page</b> to have a taste of the things I've been up to.
           </p>
         </article>
         <article className="text-box sectioned">
           <FontAwesomeIcon className="icon grow" icon={faMessage} />
           <p>
-            You can get in touch through my <b>LinkedIn</b> or <b>Instagram</b>,
-            all the links you need are up top!
+            You can get in touch through my <b>LinkedIn</b> or <b>Instagram</b>, or even better, find my <b>email</b>
+            {" "} attached to my resume. All the links you need are up top!
           </p>
         </article>
       </section>
